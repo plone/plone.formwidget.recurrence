3874dba504786b8f4f7444cf467931f2e924951b
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:26:43 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='Plone Event Recurrence Date Time Widget Archetypes z3c.form',
     author='Plone Foundation',
