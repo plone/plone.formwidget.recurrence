7329c63ac486201afaef203cab6724713ea205ed
David Glick <david@glicksoftware.com>
2025-11-10 21:28:03 -0800

    Remove setuptools from install_requires

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     python_requires=">=3.10",
     zip_safe=False,
     install_requires=[
-        "setuptools",
         "plone.base",
         "Products.GenericSetup",
         "python-dateutil",
